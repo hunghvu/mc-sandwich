1bf186243a278625beeae1a78da952030af313aa
Hung Vu <hunghvu2017@gmail.com>
2021-02-12 22:03:54 -0800

    Attempt fix deployment error H10

--- index.js
+++ index.js
@@ -15,23 +15,14 @@ app.use(express.static("assets")); // Serve static
 app.use(express.static("js"));
 app.use(express.static("css"));
 // There is no complex interaction as of now (not as webservice), so I don't decompose.
-app.get("/", (request, response) => {response.sendFile(path.join(__dirname + "/index.html"))});
-app.get("/menu", (request, response) => {response.sendFile(path.join(__dirname + "/menu.html"))});
-app.get("/order", (request, response) => {response.sendFile(path.join(__dirname + "/order.html"))});
-app.get("/cart", (request, response) => {response.sendFile(path.join(__dirname + "/cart.html"))});
+app.get("/", (request, response) => { response.sendFile(path.join(__dirname + "/index.html")) });
+app.get("/menu", (request, response) => { response.sendFile(path.join(__dirname + "/menu.html")) });
+app.get("/order", (request, response) => { response.sendFile(path.join(__dirname + "/order.html")) });
+app.get("/cart", (request, response) => { response.sendFile(path.join(__dirname + "/cart.html")) });
 
 
 
-/* 
-* Heroku will assign a port you can use via the 'PORT' environment variable
-* To access an environment variable, use process.env.<ENV>
-* If there isn't an environment variable, process.env.PORT will be null (or undefined)
-* If a value is 'falsy', i.e. null or undefined, javascript will evaluate the rest of the 'or'
-* In this case, we assign the port to be 5000 if the PORT variable isn't set
-* You can consider 'let port = process.env.PORT || 5000' to be equivalent to:
-* let port; = process.env.PORT;
-* if(port == null) {port = 5000} 
-*/ 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server up and running on port: " + (process.env.PORT || 5000));
+// Attempt to fix code H10: https://stackoverflow.com/questions/14322989/first-heroku-deploy-failed-error-code-h10
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
