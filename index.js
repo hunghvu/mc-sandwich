8a514b03bef7d51e6de973386f0d61c757fcca79
Hung Vu <hunghvu2017@gmail.com>
2021-02-12 22:30:53 -0800

    Attempt 5 to fix H10

--- index.js
+++ index.js
@@ -13,9 +13,15 @@ let path = require("path"); // To form an absolute path.
 
 app.use(express.static("assets")); // Serve static 
 app.use(express.static("js"));
+
+app.get("/", (request, response) => {
+    response.writeHead(200, {"Content-Type": "text/html" });
+    response.write("<h1> Test deployment </h1");
+    response.end();
+})
 // app.use(express.static("css"));
 // There is no complex interaction as of now (not as webservice), so I don't decompose.
-app.get("/", (request, response) => { response.sendFile(path.join(__dirname + "/index.html")) });
+// app.get("/", (request, response) => { response.sendFile(path.join(__dirname + "/index.html")) });
 // app.get("/menu", (request, response) => { response.sendFile(path.join(__dirname + "/menu.html")) });
 // app.get("/order", (request, response) => { response.sendFile(path.join(__dirname + "/order.html")) });
 // app.get("/cart", (request, response) => { response.sendFile(path.join(__dirname + "/cart.html")) });
