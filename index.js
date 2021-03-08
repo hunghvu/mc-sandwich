05ffe0cbf6681b87ac8a85f7b5046d49e6ad1b1f
Hung Vu <hunghvu2017@gmail.com>
2021-03-08 15:46:57 -0800

    Adjustment to index.js to deploy server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 //Create a new instance of express
-const app = express()
+const app = express();
 
 /*
  * This (3rd party) middleware function parses the HTTP Request Body into JSON. 
@@ -8,19 +8,46 @@ const app = express()
  * function. 
  */
 app.use(require("body-parser").json());
+/*
+ * This (3rd party) middleware function parses the HTTP Request Body into JSON. 
+ * When the body exists but does not parse, an error is passed on to the next
+ * function. 
+ */
+app.use(require("body-parser").json());
+/*
+ * This middleware function parses any HTTP Request cookies into JSON
+ */
+app.use(require('cookie-parser')());
 
-let path = require("path"); // To form an absolute path.
+
+/*
+ * This project includes a user made module called middleware. It includes helpful
+ * middleware functions. 
+ */
+const middleware = require('./server/middleware');
+
+/*
+ * This middleware function will respond to improperly formed JSON in found in 
+ * an HTTP Request BODY.
+ */
+app.use(middleware.jsonError);
+
+
+// Backend
+app.use('/auth', require('./server/routes/signin.js'));
+app.use('/auth', require('./server/routes/register.js'));
+app.use('/orders', middleware.checkTokenCookies, require('./server/routes/orders.js'));
+
 
 app.use(express.static("assets")); // Serve static 
 app.use(express.static("js"));
-
-// app.get("/", (request, response) => {
-//     // response.writeHead(200, {"Content-Type": "text/html" });
-//     // response.write("<h1> Test deployment </h1");
-//     // response.end();
-// })
 app.use(express.static("css"));
-// There is no complex interaction as of now (not as webservice), so I don't decompose.
+
+
+/**
+ * Form an absolute path.
+ */
+const path = require("path"); 
 app.get("/", (request, response) => { response.sendFile(path.join(__dirname + "/index.html")) });
 app.get("/menu", (request, response) => { response.sendFile(path.join(__dirname + "/menu.html")) });
 app.get("/order", (request, response) => { response.sendFile(path.join(__dirname + "/order.html")) });
