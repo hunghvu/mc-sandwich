aa79137c5b746d4712b40da849adcb6081d93d05
Hung Vu <hunghvu2017@gmail.com>
2021-03-15 22:18:44 -0700

    Add some momments

--- web_restricted/js/profilepage.js
+++ web_restricted/js/profilepage.js
@@ -1,3 +1,6 @@
+/**
+ * This script provide functionality for the profile page
+ */
 window.onload = () => {
     displayUsername();
     getPreviousOrders();
@@ -6,7 +9,7 @@ window.onload = () => {
 /**
  * This function get a logged user's previous orders.
  */
- async function getPreviousOrders() {
+async function getPreviousOrders() {
     let response = await fetch("../orders", {
         method: "GET"
     })
@@ -14,44 +17,44 @@ window.onload = () => {
     if (response.ok) { // if HTTP-status is 200-299
         // get the response body (the method explained below)
         let json = await response.json()
-        if(json.orders) {
+        if (json.orders) {
             console.log(json.orders);
             let orderHistoryTab = $("#order-history-tab");
             json.orders.forEach(element => {
                 let orderString =
-                    "<p id='orderid-" + element.orderid + "' name='previous-order'><img src='../image/order-remove.svg' onclick='deletePreviousOrders(" 
+                    "<p id='orderid-" + element.orderid + "' name='previous-order'><img src='../image/order-remove.svg' onclick='deletePreviousOrders("
                     + element.orderid + ")'> <b>Order name:</b> "
-                    +element["order_name"]
-                    +", <b>bread: </b>"
-                    +element.bread
-                    +", <b>meat: </b>"
-                    +element.meat
-                    +", <b>quantity: </b>"
-                    +element.quantity
-                    +", <b>size: </b>"
-                    +element.size;
-            orderString += ", <b>topping: </b>";
-            if(element["topping_crumbled_feta"]) orderString += "Crumbled Feta, ";
-            if(element["topping_mozzarella_cheese"]) orderString += "Mozzarella cheese, ";
-            if(element["topping_natural_cheddar_cheese"]) orderString += "Natural Cheddar cheese, ";
-            if(element["topping_not_choose"]) orderString += "None, ";
-            if(element["topping_old_english_cheese"]) orderString += "Old English cheese, ";
-            if(element["topping_pickle"]) orderString += "Pickle, ";
-            if(element["topping_shredded_carrot"]) orderString += "Shredded Carrot, ";
-            if(element["topping_spinach"]) orderString += "Spinach, ";
-            if(element["topping_sprout"]) orderString += "Sprout, ";
-            orderString += "<b>special topping: </b>"
-            if(element["special_topping_garlic_aioli"]) orderString += "Garlic Aioli, ";
-            if(element["special_topping_habanero_hot_sauce"]) orderString += "Hanebaro Hot Sauce, "
-            if(element["special_topping_mayonnaise"]) orderString += "Mayonnaise, "
-            if(element["special_topping_not_choose"]) orderString += "None, "
-            if(element["special_topping_smokey_bbq"]) orderString += "Smokey BBQ, "
-            orderString += "<b>Total: </b>$" + element["total_usd"] + "</p><br>";
-            orderHistoryTab.append(orderString);
+                    + element["order_name"]
+                    + ", <b>bread: </b>"
+                    + element.bread
+                    + ", <b>meat: </b>"
+                    + element.meat
+                    + ", <b>quantity: </b>"
+                    + element.quantity
+                    + ", <b>size: </b>"
+                    + element.size;
+                orderString += ", <b>topping: </b>";
+                if (element["topping_crumbled_feta"]) orderString += "Crumbled Feta, ";
+                if (element["topping_mozzarella_cheese"]) orderString += "Mozzarella cheese, ";
+                if (element["topping_natural_cheddar_cheese"]) orderString += "Natural Cheddar cheese, ";
+                if (element["topping_not_choose"]) orderString += "None, ";
+                if (element["topping_old_english_cheese"]) orderString += "Old English cheese, ";
+                if (element["topping_pickle"]) orderString += "Pickle, ";
+                if (element["topping_shredded_carrot"]) orderString += "Shredded Carrot, ";
+                if (element["topping_spinach"]) orderString += "Spinach, ";
+                if (element["topping_sprout"]) orderString += "Sprout, ";
+                orderString += "<b>special topping: </b>"
+                if (element["special_topping_garlic_aioli"]) orderString += "Garlic Aioli, ";
+                if (element["special_topping_habanero_hot_sauce"]) orderString += "Hanebaro Hot Sauce, "
+                if (element["special_topping_mayonnaise"]) orderString += "Mayonnaise, "
+                if (element["special_topping_not_choose"]) orderString += "None, "
+                if (element["special_topping_smokey_bbq"]) orderString += "Smokey BBQ, "
+                orderString += "<b>Total: </b>$" + element["total_usd"] + "</p><br>";
+                orderHistoryTab.append(orderString);
 
             });
         }
-        if(json.message) console.log(json.message);
+        if (json.message) console.log(json.message);
         // alert(json.orders);
 
     } else {
@@ -61,6 +64,9 @@ window.onload = () => {
     }
 }
 
+/**
+ * This function delete a logged user's previous order.
+ */
 async function deletePreviousOrders(orderid) {
     // console.log(orderid)
     let response = await fetch("../orders", {
